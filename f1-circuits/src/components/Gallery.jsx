4dbf68ee91142f33a00079ceb4aa3bdf430c376e
Rsarm09 <rnasarmiento@gmail.com>
2025-04-02 13:11:02 -0700

    update

--- f1-circuits/src/components/Gallery.jsx
+++ f1-circuits/src/components/Gallery.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
+import './Gallery.css';
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+//images
+import MonzaPoster from '/images/MonzaPoster.png';
+import MonacoPoster from '/images/MonacoPoster.png';
+import SilverstonePoster from '/images/SilverstonePoster.png';
+
+
+
 
 export default function Gallery() {
+  const galleryItems = [
+    { id: 1, name: 'monza', image: MonzaPoster },
+    { id: 2, name: 'monaco', image: MonacoPoster },
+    { id: 3, name: 'silverstone', image: SilverstonePoster },
+    { id: 4, name: 'monza', image: MonzaPoster },
+    { id: 5, name: 'monza', image: MonzaPoster }
+  ];
+
+  const imageContainerRef = useRef(null);
+  const imageRefs = useRef([]);
+
+  useEffect(() => {
+    let totalWidth = 0;
+
+    imageRefs.current.forEach((img) => {
+      if (img) totalWidth += img.offsetWidth;
+    });
+
+    gsap.to(imageContainerRef.current, {
+      x: -totalWidth + window.innerWidth, 
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: '.gallery-section',
+        start: 'top top',
+        end: () => `+=${totalWidth}`, 
+        scrub: 3,
+        pin: true
+      }
+    });
+  }, []);
+
   return (
-    <div>e</div>
-  )
+    <section className="gallery-section">
+      <div className="gallery-container" ref={imageContainerRef}>
+        {galleryItems.map((item, index) => (
+          <div key={item.id} className="gallery-item">
+            <img
+              src={item.image}
+              alt={item.name}
+              className="gallery-image"
+              ref={(el) => (imageRefs.current[index] = el)} // Assign each img a ref
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
--- f1-circuits/src/pages/Home.jsx
+++ f1-circuits/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import '../global.css';
+import './Home.css';
 
 import Hero from '../components/Hero';
 import InfoNumbers from '../components/InfoNumbers';
 import Paragraph from '../components/Paragraph';
+import Gallery from '../components/Gallery';
+import TitleCardComponent from '../components/TitleCard';
 
 
 export default function Home() {
@@ -12,6 +15,10 @@ export default function Home() {
             <Hero />
             <InfoNumbers />
             <Paragraph />
+            <Gallery />
+            <div className='title-card'>
+                <TitleCardComponent name='monza' location='Autodromo Nazionale di Monza' year='1921'/>
+            </div>
         </div>
     )
 }
